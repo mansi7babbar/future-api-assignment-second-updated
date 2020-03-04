54bc4e558e39ee2b953817eb58db74387082795b
mansi7babbar <mansi7babbar@gmail.com>
2020-03-04 06:50:16 +0530

    updated test cases

--- src/test/scala/com/knoldus/JsonParsingSpec.scala
+++ src/test/scala/com/knoldus/JsonParsingSpec.scala
@@ -1,8 +1,21 @@
 package com.knoldus
 
-import org.scalamock.scalatest.MockFactory
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AsyncFlatSpec
 
-class JsonParsingSpec extends AnyFunSuite with MockFactory {
+class JsonParsingSpec extends AsyncFlatSpec {
 
+  behavior of "getUserWithMostPosts"
+  it should "return user having maximum posts" in {
+    JsonParsing.getUserWithMostPosts.map(postUser => assert(postUser.user.id == 1))
+  }
+
+  behavior of "getPostWithMostComments"
+  it should "return post having maximum comments" in {
+    JsonParsing.getPostWithMostComments.map(commentPost => assert(commentPost.post.id == 1))
+  }
+
+  behavior of "getUserWithPostWithMostComments"
+  it should "return user with post having maximum comments" in {
+    JsonParsing.getUserWithPostWithMostComments.map(user => assert(user.head.id == 1))
+  }
 }
